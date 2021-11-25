57b8a22e7f4dd4123269253e6241bdc6a5b03191
decadence <decadence127@gmail.com>
2021-11-26 01:32:55 +0300

    Resolved conflicts

--- Client/Electron/src/main.js
+++ Client/Electron/src/main.js
@@ -1,20 +1,8 @@
-<<<<<<< HEAD
-const { app, BrowserWindow, remote, ipcMain } = require("electron");
-=======
 const { app, BrowserWindow, ipcMain } = require("electron");
->>>>>>> BackEnd/backend_1
 const path = require("path");
 const SocketHandler = require("./ElectronComponents/SocketHandler");
 const WindowNodeHandler = require("./ElectronComponents/WindowNodeHandler");
 
-<<<<<<< HEAD
-ipcMain.handle("asyncAction", async (event, arg) => {
-  const response = await SocketHandler(arg);
-  console.log("Response main:", response);
-  return response;
-});
-=======
->>>>>>> BackEnd/backend_1
 if (require("electron-squirrel-startup")) {
   app.quit();
 }
@@ -26,31 +14,16 @@ const createWindow = () => {
     height: 720,
     frame: false,
     webPreferences: {
-<<<<<<< HEAD
-      nodeIntegration: true,
-      contextIsolation: true,
-      enableRemoteModule: true,
-      preload: path.join(__dirname, "preload.js"),
-=======
       nodeIntegration: false,
       contextIsolation: true,
       enableRemoteModule: true,
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
->>>>>>> BackEnd/backend_1
     },
   });
 
   mainWindow.loadURL(
     path.join(__dirname, "./ElectronComponents/MainLoadingPage/loading.html")
   );
-<<<<<<< HEAD
-  console.log(remote);
-  setTimeout(() => {
-    mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
-  }, 2500);
-};
-
-=======
 
   setTimeout(() => {
     mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
@@ -63,7 +36,6 @@ const createWindow = () => {
 //   return response;
 // });
 
->>>>>>> BackEnd/backend_1
 app.on("ready", createWindow);
 
 ipcMain.on("toMain", (event, args) => {
